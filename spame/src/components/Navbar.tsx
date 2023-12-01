1677a7e8f1546f3b59ffb5edc50b6798ea308496
gustavo de assis <gustavo.siqueira.421@gmail.com>
2023-12-01 15:16:43 -0300

    feat: showing user info on navbar

--- spame/src/components/Navbar.tsx
+++ spame/src/components/Navbar.tsx
@@ -1,15 +1,26 @@
+"use client";
+import { AuthContext } from "@/app/context/authContext";
 import Image from "next/image";
+import { useContext, useEffect, useState } from "react";
 
 export default function Navbar() {
+  const { user } = useContext(AuthContext);
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    setUserName(user.name.split(" ")[0]);
+  }, [userName]);
+
   return (
     <main>
       <nav className="flex flex-row bg-[#071952] h-16 justify-between items-center px-3 fixed top-0 right-0 z-10 w-full">
-        <div>
+        <div className=" gap-1">
           <Image src={"/assets/logo.png"} width={60} height={38} alt="logo" />
+          <p>{user.role.toUpperCase()}</p>
         </div>
 
         <div className="flex gap-3 font-semibold text-lg text-[#A5F1E9]">
-          <p>Olá Beltrano!</p>
+          <p>Olá {userName}!</p>
           <p>Logout</p>
         </div>
       </nav>
